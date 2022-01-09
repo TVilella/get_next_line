bedee831a9eadb0446084ee8062dc41e562349cc
TVilella <vilelladev@outlook.com>
2022-01-09 19:36:02 +0100

    add utils and header

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,25 @@
 /*   By: tcarvalh <tcarvalh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 19:34:46 by tcarvalh          #+#    #+#             */
-/*   Updated: 2022/01/09 05:54:51 by tcarvalh         ###   ########.fr       */
+/*   Updated: 2022/01/09 19:35:11 by tcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include <unistd.h>
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
+#  define BUFFER_SIZE 1
 # endif
 
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *str);
-char	ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,87 @@
 /*   By: tcarvalh <tcarvalh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 19:34:44 by tcarvalh          #+#    #+#             */
-/*   Updated: 2022/01/09 05:54:33 by tcarvalh         ###   ########.fr       */
+/*   Updated: 2022/01/09 17:16:21 by tcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		index;
+	size_t	length;
+
+	length = ft_strlen(s);
+	if (c == 0 && length == 0)
+		return ((char *)s);
+	index = 0;
+	while (*(s + index) != '\0')
+	{
+		if (*(s + index) == (unsigned char)c)
+			return ((char *)(s + index));
+		index++;
+	}
+	if (*(s + index) == (unsigned char)c)
+		return ((char *)(s + index));
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ptr;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	ptr = (char *)malloc(len + 1);
+	if (ptr != NULL)
+		ft_strlcpy(ptr, s1, len + 1);
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new = (char *)malloc(s1_len + s2_len + 1);
+	if (new != NULL)
+	{
+		ft_strlcpy(new, s1, s1_len + 1);
+		ft_strlcpy(new + s1_len, s2, s2_len + 1);
+	}
+	return (new);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	l_src;
+	size_t	index;
+
+	l_src = ft_strlen(src);
+	index = 0;
+	if (dstsize == 0)
+		return (l_src);
+	while (index < dstsize - 1 && *(src + index))
+	{
+		*(dst + index) = *(src + index);
+		index++;
+	}
+	*(dst + index) = '\0';
+	return (l_src);
+}
