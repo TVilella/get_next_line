b7ba13004c098b0f135acf021e68eba8bad430ed
TVilella <vilelladev@outlook.com>
2022-05-30 15:36:44 +0200

    Falta colocar dentro da norma

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tcarvalh <tcarvalh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 19:34:40 by tcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/29 21:14:09 by tcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/30 15:34:27 by tcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,86 +15,93 @@
 
 static char	*read_until_nl(int fd, char *str)
 {
-	char	*buf;
-	char	*tmp;
-	ssize_t	size;
+	ssize_t		size;
+	char		*tmp;
+	char		*buffer;
+	int			nl;
 
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf)
-		return (NULL);
 	size = BUFFER_SIZE;
-	while (!ft_strchr(str, '\n') && size == BUFFER_SIZE)
+	nl = 0;
+	buffer = (char *)malloc(sizeof (char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	while (!nl && size == BUFFER_SIZE)
 	{
-		size = read(fd, buf, BUFFER_SIZE);
+		size = read(fd, buffer, BUFFER_SIZE);
 		if (size == -1)
 		{
-			free(buf);
+			free(buffer);
 			return (NULL);
 		}
-		buf[size] = '\0';
+		buffer[size] = '\0';
+		if (!str)
+			str = ft_strdup("");
 		tmp = str;
-		str = ft_strjoin(str, buf);
+		str = ft_strjoin(tmp, buffer);
 		free(tmp);
+		if (ft_strchr(str, '\n'))
+			nl = 1;
 	}
-	free(buf);
+	free(buffer);
 	return (str);
 }
 
 static char	*save_remainder(char *str)
 {
+	int		index;
 	char	*remainder;
 	size_t	len;
-	size_t	index;
 
 	index = 0;
-	len = ft_strlen(str);
-	while (str[index] != '\0' && str[index] != '\n')
+	while (str[index] && str[index] != '\n')
 		index++;
-	if (!str[index])
+	if (str[index] == '\0')
+	{
+		free(str);
 		return (NULL);
-	remainder = (char *)malloc(sizeof (char) * (len - index + 1));
+	}
+	len = ft_strlen(str);
+	remainder = (char *)malloc(sizeof (char) * len - index + 1);
 	if (!remainder)
 		return (NULL);
 	ft_strlcpy(remainder, str + index + 1, len - index + 1);
+	free(str);
 	return (remainder);
 }
 
 static char	*extract_line(char *str)
 {
+	int		i;
 	char	*line;
-	size_t	pos;
-	size_t	len;
 
-	len = ft_strlen(str);
-	pos = len - ft_strlen(ft_strchr(str, '\n'));
-	line = (char *)malloc(sizeof(char) * (pos + 1));
-	ft_strlcpy(line, str, pos + 1);
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof (char) * (i + 2));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, str, i + 2);
+	if (line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*prev_str = NULL;
-	char		*str;
 	char		*line;
+	static char	*str;
 
-	line = NULL;
-	if( fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!prev_str)
-		str = ft_strdup("");
-	else
-		str = ft_strdup(prev_str);
 	str = read_until_nl(fd, str);
 	if (!str)
 		return (NULL);
-	prev_str = save_remainder(str);
-	if (prev_str == NULL)
-	{
-		free(prev_str);
-		return (str);
-	}
 	line = extract_line(str);
-	free(str);
+	str = save_remainder(str);
 	return (line);
 }
